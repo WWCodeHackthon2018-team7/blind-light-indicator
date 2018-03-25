be70df3ad3c0574d161ecd6ae17b2422eadb2108
Holly <holly.wilkalis@gmail.com>
2018-03-25 12:55:15 -0700

    Update light-indicator.ino
    Incorporate a new time interval and logic to change position based on temp and light values

--- light-indicator/light-indicator.ino
+++ light-indicator/light-indicator.ino
@@ -25,6 +25,8 @@ Servo servo;
 int angle = 0;   // servo position in degrees
 //Indicate which pin on the board
 int photocellPin = 0;
+long previousMillis = 0; //stores last time state was updated
+long interval = 60000; //creates time interval
 void setup() {
   Serial.begin(112500);
   
@@ -56,59 +58,66 @@ void setup() {
   
 }
 void loop() {
+  //creating a time interval
+  unsigned long currentMillis = millis();
+  if(currentMillis - previousMillis > interval) {
+    previousMillis = currentMillis;
   
-  Serial.print("Connecting to ");
-  Serial.println(host);
-  
-  // Use WiFiClient class to create TCP connections
-  WiFiClient client;
-  const int httpPort = 80;
-  //Error message if connection failed
-  if (!client.connect(host, httpPort)) {
-    Serial.println("connection failed");
-    return;
-  }
-  //Read value from photocell
-  int photoValue = analogRead(photocellPin);
-   // Reading temperature and humidity
-  float h = dht.readHumidity();
-  // Read temperature as Celsius
-  float t = dht.readTemperature();
-  //If the value of the photcell reads below 900, indicating change in light
-  if (photoValue < 900 || t > 31) {
-    //Turn the Servo a full rotation
-    angle = 180;
-  } else {
-    angle = 0;
+    Serial.print("Connecting to ");
+    Serial.println(host);
+    
+    // Use WiFiClient class to create TCP connections
+    WiFiClient client;
+    const int httpPort = 80;
+    //Error message if connection failed
+    if (!client.connect(host, httpPort)) {
+      Serial.println("connection failed");
+      return;
     }
-  // This will send the request to the server
-  // Include the values of the temperature, humidity and light sensor
-  client.print(String("GET /dweet/for/wwc18iotteam7?temperature=") + String(t) + "&humidity=" + String(h) + "&lumens=" + String(photoValue) + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" + 
-               "Connection: close\r\n\r\n");
-  delay(10);
-  
-  // Read all the lines of the reply from server and print them to Serial
-  while(client.available()){
-    String line = client.readStringUntil('\r');
-    Serial.print(line);
+    //Read value from photocell
+    int photoValue = analogRead(photocellPin);
+     // Reading temperature and humidity
+    float h = dht.readHumidity();
+    // Read temperature as Celsius
+    float t = dht.readTemperature();
+    //If the the temperature value drops below 5C, close the blinds
+    if  { (t < 5)
+      //Turn the Servo a full rotation
+      angle = 180;
+       //OR if the value of the photcell reads below 600 or the temperature is above 31C, close the blinds
+    } else if (photoValue > 600 || t > 31) {
+        angle = 180;
+      } else {
+      angle = 0;
+      }
+    // This will send the request to the server
+    // Include the values of the temperature, humidity and light sensor
+    client.print(String("GET /dweet/for/wwc18iotteam7?temperature=") + String(t) + "&humidity=" + String(h) + "&lumens=" + String(photoValue) + " HTTP/1.1\r\n" +
+                 "Host: " + host + "\r\n" + 
+                 "Connection: close\r\n\r\n");
+    delay(10);
+    
+    // Read all the lines of the reply from server and print them to Serial
+    while(client.available()){
+      String line = client.readStringUntil('\r');
+      Serial.print(line);
+    }
+    
+    Serial.println();
+    Serial.println("closing connection");
+    // Display data
+    Serial.print("Humidity: ");
+    Serial.print(h);
+    Serial.print(" %\t");
+    Serial.print("Temperature: ");
+    Serial.print(t);
+    Serial.println(" *C ");
+    Serial.println("\n");
+    Serial.println(photoValue);
+    Serial.println("\n");
+    //Move the Servo to its new angle!
+    servo.write(angle);
+    //Delay the sensor 2 second 
+    delay(2000);
   }
-  
-  Serial.println();
-  Serial.println("closing connection");
-  // Display data
-  Serial.print("Humidity: ");
-  Serial.print(h);
-  Serial.print(" %\t");
-  Serial.print("Temperature: ");
-  Serial.print(t);
-  Serial.println(" *C ");
-  Serial.println("\n");
-  Serial.println(photoValue);
-  Serial.println("\n");
-  //Move the Servo to its new angle!
-  servo.write(angle);
-  //Delay the sensor 2 second 
-  delay(2000);
 }
-
